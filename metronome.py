5a6fd78e819fbdd50327e724583028b31123214f
Administrator <astrikol@gmail.com>
2022-04-11 16:14:07 +0300

    Built a metronome.

---
+++ metronome.py
@@ -0,0 +1,44 @@
+import time
+import simpleaudio
+
+
+def wait(delay):
+    end_time = time.time() + delay
+    while end_time > time.time():
+        continue
+
+
+def metronome(bpm):
+    print(float(bpm), "BPM")
+    delay = 60 / bpm
+    count = 0
+    beat = 0
+    mode = 4
+
+    while True:
+        wait(delay)
+        count += 1
+
+        if count > mode:
+            count += 1
+            beat += 1
+
+        wave_obj = simpleaudio.WaveObject.from_wave_file('metronome.wav')
+        if count == 1:
+            wave_obj = simpleaudio.WaveObject.from_wave_file('metronomeup.wav')
+
+        play_obj = wave_obj.play()
+        play_obj
+
+
+while True:
+    beats = input("Input bpm or 'q' to stop: ")
+    if beats == 'q':
+        break
+    else:
+        try:
+            beats = int(beats)
+            metronome(beats)
+        except ValueError:
+            print("Invalid input, please input a valid bpm.")
+        continue
